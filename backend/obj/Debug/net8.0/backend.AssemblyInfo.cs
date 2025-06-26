6a78a8d36a36e78653c53af20b9c6ca10a3c215e
Ivar Gavelin <ivargavelin@gmail.com>
2025-06-26 13:07:05 +0300

    English Translation

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9191f0ee456cb45cc13a5012a6632ff51d733c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56b74ff83eea457790f3c8647a492004716d6e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
