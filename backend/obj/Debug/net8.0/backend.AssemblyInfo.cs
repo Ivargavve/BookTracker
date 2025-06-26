d68548064d581cea52dbe4a3674a461079e21c82
Ivar Gavelin <ivargavelin@gmail.com>
2025-06-26 11:23:31 +0300

    ready for hosting

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a39250aafc981bc215bc8f7cbf405f1879d1e80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7d42c29be40ce4a794d40bb11d06cc79d633ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
